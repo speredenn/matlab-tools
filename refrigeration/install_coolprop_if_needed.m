27a42579345fff3bc4924b610bc07fb33660895e
Jean-Baptiste Carré (speredenn) <jbc@0x1d.org>
2015-10-13 23:55:37 +0200

    Added a function to install CoolProp directly inside a MATLAB project automatically, if CoolProp not installed already. This needs to be updated with tests of the proper installation with calculations.

---
+++ refrigeration/install_coolprop_if_needed.m
@@ -0,0 +1,48 @@
+function [flag,status] = install_coolprop_if_needed(cpVersion, folderLocation)
+
+
+if exist(folderLocation, 'dir') ~= 7
+    
+    originLocation = pwd;
+    mkdir(folderLocation);
+
+    cpWrapperFileNames = { ...
+        '+CoolProp.7z'
+        'CoolPropMATLAB_wrap.mexw32'
+        'Example.m'
+        'CoolPropsetup.m'
+        'SwigRef.m'
+        'CoolPropMATLAB_wrap.mexw64'
+        'CoolPropMATLAB_wrap.mexa64'
+        'CoolPropMATLAB_wrap.mexmaci64'
+        };
+
+    cpBaseUrl = ['http://downloads.sourceforge.net/project/coolprop/CoolProp/' cpVersion '/MATLAB'];
+    cpWrapperFileUrls = cellfun(@(x) [cpBaseUrl '/' x], cpWrapperFileNames, ...
+        'UniformOutput', 0);
+    
+    fprintf('Downloading CoolProp %s...\n', cpVersion)
+
+    cd(folderLocation)
+    
+    for i = 1: length(cpWrapperFileUrls)
+        urlwrite(cpWrapperFileUrls{i}, cpWrapperFileNames{i});
+    end
+    
+    mkdir('+CoolProp')
+    movefile('+CoolProp.7z', '+Coolprop/+CoolProp.7z')
+    cd('+CoolProp')
+    [flag,status] = system(['"C:\Program Files\7-Zip\7z.exe" e ' ...
+        [pwd '\+CoolProp.7z'] ' >null']);
+    
+    % Cleaning up
+    delete('null', '+CoolProp.7z')
+    
+    fprintf('CoolProp %s has been installed.\n', cpVersion)
+    cd(originLocation)
+    
+end
+
+
+end
+
